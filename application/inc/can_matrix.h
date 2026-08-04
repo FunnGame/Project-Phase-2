863b9df27507e1568e4549c81013779942044cee
Nguyen Duc Trong <23020892@vnu.edu.vn>
2026-08-04 09:12:02 +0700

    update can_matrix

--- application/inc/can_matrix.h
+++ application/inc/can_matrix.h
@@ -1,47 +1,36 @@
+/*
+ * can_matrix.h
+ *
+ * Author: trong
+ */
+
 #ifndef CAN_MATRIX_H
 #define CAN_MATRIX_H
 
 #include <stdint.h>
+#include "rf_protocol.h"
 
-#define CAN_ID_CONTROL_CMD        0x100
-#define CAN_ID_SENSOR_DISTANCE    0x200
-#define CAN_ID_TELEMETRY_STATUS   0x300
-#define CAN_ID_SYSTEM_POST        0x400
-
-typedef enum
-{
-    MODE_MANUAL = 0,
-    MODE_AUTO   = 1
-} SystemMode_t;
+/*=========================================================
+ * Vehicle Command
+ *========================================================*/
 
 typedef struct
 {
     int8_t throttle;
     int8_t steering;
+
     uint8_t mode;
-    uint8_t flags;
-    uint8_t seq;
-    uint8_t crc;
-} CAN_ControlCmd_t;
 
-typedef struct
-{
-    uint16_t distance_mm;
-} CAN_SensorDistance_t;
+} VehicleCommand_t;
 
-typedef struct
-{
-    uint16_t speed_rpm;
-    uint8_t acc_active;
-    uint8_t aeb_active;
-    uint8_t battery_percent;
-} CAN_TelemetryStatus_t;
+/*=========================================================
+ * API
+ *========================================================*/
 
-typedef struct
-{
-    uint8_t rf_ok;
-    uint8_t can_ok;
-    uint8_t spi_ok;
-} CAN_PostStatus_t;
+void CANMatrix_Init(void);
+
+void CANMatrix_Update(void);
+
+VehicleCommand_t CANMatrix_GetCommand(void);
 
 #endif
--- application/src/can_matrix.c
+++ application/src/can_matrix.c
@@ -1,3 +1,34 @@
+/*
+ * can_matrix.c
+ *
+ * Author: trong
+ */
+
 #include "can_matrix.h"
 
-/* Tạm thời chưa code gì ở đây */
+static VehicleCommand_t command;
+void CANMatrix_Init(void)
+{
+    command.throttle = 0;
+    command.steering = 0;
+    command.mode = 0;
+}
+VehicleCommand_t CANMatrix_GetCommand(void)
+{
+    return command;
+}
+void CANMatrix_Update(void)
+{
+    RF_ControlPacket_t packet;
+
+    if(!RF_IsConnected())
+    {
+        return;
+    }
+
+    packet = RF_GetControl();
+
+    command.throttle = packet.throttle;
+    command.steering = packet.steering;
+    command.mode = packet.mode;
+}
